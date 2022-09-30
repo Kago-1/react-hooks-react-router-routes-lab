a71d4cadda11666900add74eb866572a3301a126
Steve <stephenkago10@gmail.com>
2022-10-01 02:03:54 +0300

    add each actor's name and movies

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,18 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const eachactor = actors.map((actor) => (
+    <div key={actor.name}>
+     <h3>{actor.name}</h3>
+     <ul>
+      {actor.movies.map((movie)=>(<li key={movie}>{movie}</li>))}
+     </ul>
+    </div>
+  ))
+  return <div>{/*{code here}*/}
+  <h1>Actors Page</h1>
+  {eachactor}
+  </div>;
 }
 
 export default Actors;
